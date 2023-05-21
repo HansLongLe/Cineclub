113cbe60c415607ffe4d8ce3e7f5eb9caf726592
Jan Le <74683794+HansLongLe@users.noreply.github.com>
2023-05-21 14:40:55 +0200

    fixing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./common/router";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 test("renders learn react link", () => {
-  render(<RouterProvider router={router} />);
+  render(
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>,
+  );
 });
